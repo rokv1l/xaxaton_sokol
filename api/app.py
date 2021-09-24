844a95866d1937cba491b85483aeca1e31f6935d
Leonid <66663007+rokv1l@users.noreply.github.com>
2021-09-24 23:35:36 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
 from resources.hello import Hello
@@ -6,6 +7,7 @@ from resources.route import Route
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(Hello, '/hello')
@@ -13,4 +15,4 @@ api.add_resource(Route, '/route')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
