2eb784452032efa057a02aaf060afb25b1714298
leo <guzeev.leonid.dc@gmail.com>
2021-09-24 23:48:55 +0300

    done sensors init (addition)

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
+from utils.init_sensors import init_sensors
 from resources.hello import Hello
 from resources.route import Route
 
@@ -15,4 +16,5 @@ api.add_resource(Route, '/route')
 
 
 if __name__ == '__main__':
+    init_sensors()
     app.run(host='0.0.0.0', port=5000, debug=True)
