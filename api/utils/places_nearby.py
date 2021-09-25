3d857679d9ab752c0ffec35e5a1ff372b1184d34
leo <guzeev.leonid.dc@gmail.com>
2021-09-25 22:12:08 +0300

    places and bike base nearby point(fix)

--- api/utils/places_nearby.py
+++ api/utils/places_nearby.py
@@ -6,7 +6,7 @@ from src.bike_base import BikeBase
 
 
 def haversine(lat1, lng1, lat2, lng2):
-    R = 6372.8  # this is in km
+    R = 6371.0  # this is in km
 
     dLat = radians(lat2 - lat1)
     dLng = radians(lng2 - lng1)
@@ -24,12 +24,14 @@ def get_places_nearby(lat, lng, radius):
         places = session.query(InterestPlace).all()
         result = []
         for place in places:
-            if haversine(lat, lng, place.lat, place.lng) * 1000 < radius:
+            length = haversine(lat, lng, place.lat, place.lng) * 1000
+            if length < radius:
                 result.append({
                     'name': place.name,
                     'lat': place.lat,
                     'lng': place.lng,
-                    'img': place.img
+                    'img': place.img,
+                    'lenth': length
                 })
     return result
 
@@ -39,11 +41,13 @@ def get_bike_bases_nearby(lat, lng, radius):
         bike_bases = session.query(BikeBase).all()
         result = []
         for base in bike_bases:
-            if haversine(lat, lng, base.lat, base.lng) * 1000 < radius:
+            length = haversine(lat, lng, base.lat, base.lng) * 1000
+            if length < radius:
                 result.append({
                     'address': base.address,
                     'lat': base.lat,
                     'lng': base.lng,
-                    'is_open': base.is_open
+                    'is_open': base.is_open,
+                    'lenth': length
                 })
     return result
