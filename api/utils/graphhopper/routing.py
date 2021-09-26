50ae4f099bd62b09ab5775e57c9ac6587e62eeca
Leonid <66663007+rokv1l@users.noreply.github.com>
2021-09-26 03:18:11 +0300

    Update routing.py

--- api/utils/graphhopper/routing.py
+++ api/utils/graphhopper/routing.py
@@ -76,7 +76,10 @@ def get_routes(points, vehicle='foot', block_areas=None, alternative_routes=None
     routes = []
     for path in data['paths']:
         route = {
-            'waypoints': mark_waypoints(path['points']['coordinates']),
+            'waypoints': [
+                "waypoint": mark_waypoints(path['points']['coordinates']),
+                "color": "#007BFF"
+            ],
             'dist': path['distance'],
             'time': path['time'],
         }
