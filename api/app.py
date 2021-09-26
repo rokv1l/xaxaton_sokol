360183c843e9a984b61530e7ef3bb48f9b10cd0c
leo <guzeev.leonid.dc@gmail.com>
2021-09-26 03:43:16 +0300

    feature

--- api/app.py
+++ api/app.py
@@ -3,6 +3,10 @@ from flask_cors import CORS
 from flask_restful import Api
 
 from utils.init_sensors import init_sensors
+from utils.aggregation import aggregate_sensors_data, distribute_coords
+from src.db import session_maker
+from src.sensors import Sensor
+from src.aggregated_sensors import AggregatedSensor
 from resources.hello import Hello
 from resources.route import Route
 from resources.zones import Zones
@@ -20,7 +24,10 @@ api.add_resource(Zones, "/zones")
 
 
 if __name__ == '__main__':
-    # init_sensors()
-    # aggregate_sensors_data(2)
-    # add_coords_to_aggregated_sensors_data()
+    with session_maker() as session:
+        if not session.query(Sensor).first():
+            init_sensors()
+        if not session.query(AggregatedSensor).first():
+            aggregate_sensors_data()
+            distribute_coords()
     app.run(host='0.0.0.0', port=5000, debug=True)
